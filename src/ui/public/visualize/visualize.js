7c0359ad9675ee54b1d0d3d8baf9892e4e392bf4
ppisljar <peter.pisljar@gmail.com>
2018-01-31 08:54:43 +0100

    reactifying <visualization>
    adding visualization loader
    
    updating default editor
    
    updating visualize
    
    updating discover

--- src/ui/public/visualize/visualize.js
+++ src/ui/public/visualize/visualize.js
@@ -27,6 +27,7 @@ import './visualization';
 import './visualization_editor';
 import { FilterBarQueryFilterProvider } from '../filter_bar/query_filter';
 import { ResizeChecker } from '../resize_checker';
+import { visualizationLoader } from './loader';
 
 import {
   isTermSizeZeroError,
@@ -138,6 +139,11 @@ uiModules
               $scope.visData = resp;
               $scope.$apply();
               $scope.$broadcast('render');
+
+              if (!$scope.editorMode) {
+                visualizationLoader($el[0], $scope.vis, $scope.visData, $scope.uiState, { listenOnChange: true });
+              }
+
               return resp;
             });
         }, 100);
@@ -181,7 +187,11 @@ uiModules
           resizeChecker.destroy();
         });
 
-        $scope.$watch('vis.initialized', $scope.fetch);
+        if ($scope.editorMode) {
+          $scope.$watch('vis.initialized', $scope.fetch);
+        } else {
+          $scope.vis.initialized = true;
+        }
 
         $scope.fetch();
       }
