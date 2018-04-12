457713817a0dba4cf08df7190c8f2b85d982fb08
ppisljar <peter.pisljar@gmail.com>
2018-04-12 11:23:24 -0500

    removing angular from AggTypes

--- src/ui/public/agg_types/buckets/_bucket_agg_type.js
+++ src/ui/public/agg_types/buckets/_bucket_agg_type.js
@@ -1,21 +1,17 @@
 import _ from 'lodash';
-import { AggTypesAggTypeProvider } from 'ui/agg_types/agg_type';
+import { AggType } from 'ui/agg_types/agg_type';
 
-export function AggTypesBucketsBucketAggTypeProvider(Private) {
-  const AggType = Private(AggTypesAggTypeProvider);
+_.class(BucketAggType).inherits(AggType);
+function BucketAggType(config) {
+  BucketAggType.Super.call(this, config);
 
-  _.class(BucketAggType).inherits(AggType);
-  function BucketAggType(config) {
-    BucketAggType.Super.call(this, config);
-
-    if (_.isFunction(config.getKey)) {
-      this.getKey = config.getKey;
-    }
+  if (_.isFunction(config.getKey)) {
+    this.getKey = config.getKey;
   }
-
-  BucketAggType.prototype.getKey = function (bucket, key) {
-    return key || bucket.key;
-  };
-
-  return BucketAggType;
 }
+
+BucketAggType.prototype.getKey = function (bucket, key) {
+  return key || bucket.key;
+};
+
+export { BucketAggType };
--- src/ui/public/vis/agg_configs.js
+++ src/ui/public/vis/agg_configs.js
@@ -10,10 +10,10 @@
 import _ from 'lodash';
 import { IndexedArray } from 'ui/indexed_array';
 import { AggConfig } from 'ui/vis/agg_config';
-import { AggTypesIndexProvider } from 'ui/agg_types/index';
+import { aggTypes } from 'ui/agg_types/index';
 
-export function VisAggConfigsProvider(Private) {
-  AggConfig.aggTypes = Private(AggTypesIndexProvider);
+export function VisAggConfigsProvider() {
+  AggConfig.aggTypes = aggTypes;
 
   _.class(AggConfigs).inherits(IndexedArray);
   function AggConfigs(vis, configStates) {
