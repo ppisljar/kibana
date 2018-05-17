bf66026ba52083cad1cf38fe9f4bc5f1f587cd2f
Tim Roes <tim.roes@elastic.co>
2018-05-17 12:51:30 +0200

    Allow opening the inspector via loader handler

--- src/ui/public/visualize/loader/loader.js
+++ src/ui/public/visualize/loader/loader.js
@@ -82,7 +82,7 @@ const VisualizeLoaderProvider = ($compile, $rootScope, savedVisualizations) => {
       container.html(visHtml);
     }
 
-    return new EmbeddedVisualizeHandler(visHtml, scope);
+    return new EmbeddedVisualizeHandler(visHtml, scope, savedObj);
   };
 
   return {
